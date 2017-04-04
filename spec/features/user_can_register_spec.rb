a05173ec44ec71c5b47dd5178f09e023c55ff0c6
Jayson H Bucy <jayson.h.b@gmail.com>
2017-04-04 09:59:48 -0500

    fixing tests

---
+++ spec/features/user_can_register_spec.rb
@@ -0,0 +1,72 @@
+require 'rails_helper'
+
+RSpec.describe 'user registers'do
+  scenario "using google oauth2" do
+    stub_omniauth
+    visit new_user_session_path
+    expect(page).to have_link("Sign In")
+    click_link "Sign In"
+    expect(page).to have_link(href: user_google_oauth2_omniauth_authorize_path)
+    click_link href: user_google_oauth2_omniauth_authorize_path
+    expect(page).to have_content("Welcome to the GASC")
+    expect(page).to have_link("Sign Out")
+  end
+
+  scenario 'using an email' do
+    visit new_user_registration_path
+    fill_in 'user_firstname', with: 'Tester'
+    fill_in 'user_lastname', with: 'Account'
+    fill_in 'user_email', with: 'test.account@testing.test'
+    fill_in 'user_password', with: 'userpassword'
+    fill_in 'user_password_confirmation', with: 'userpassword'
+    click_button 'Sign up'
+    expect(page).to have_content('Welcome to the GASC')
+  end
+  
+  scenario 'using an existing email' do
+    stub_email
+    visit new_user_registration_path
+    fill_in 'user_firstname', with: 'Tester'
+    fill_in 'user_lastname', with: 'Account'
+    fill_in 'user_email', with: 'tester.account@testing.test'
+    fill_in 'user_password', with: 'testing01'
+    fill_in 'user_password_confirmation', with: 'testing01'
+    click_button 'Sign up'
+    expect(page).to have_content('Email has already been taken')
+  end
+
+  scenario 'using no name' do
+    visit new_user_registration_path
+    fill_in 'user_firstname', with: ''
+    fill_in 'user_lastname', with: ''
+    fill_in 'user_email', with: 'test.account@testing.test'
+    fill_in 'user_password', with: 'userpassword'
+    fill_in 'user_password_confirmation', with: 'userpassword'
+    click_button 'Sign up'
+    expect(page).to have_content('Firstname can\'t be blank')
+    expect(page).to have_content('Lastname can\'t be blank')
+  end
+
+  scenario 'using no password' do
+    visit new_user_registration_path
+    fill_in 'user_firstname', with: 'Tester'
+    fill_in 'user_lastname', with: 'Account'
+    fill_in 'user_email', with: 'test.account@testing.test'
+    fill_in 'user_password', with: ''
+    fill_in 'user_password_confirmation', with: ''
+    click_button 'Sign up'
+    expect(page).to have_content('Password can\'t be blank')
+  end
+
+  scenario 'using a short password' do
+    visit new_user_registration_path
+    fill_in 'user_firstname', with: 'Tester'
+    fill_in 'user_lastname', with: 'Account'
+    fill_in 'user_email', with: 'test.account@testing.test'
+    fill_in 'user_password', with: 'word'
+    fill_in 'user_password_confirmation', with: 'word'
+    click_button 'Sign up'
+    expect(page).to have_content('Password is too short (minimum is 6 characters)')
+  end
+
+end
